f10bb1068cbd5125fcef94759ed25669033e596b
acxa <anavillavillaana@gmail.com>
2026-02-16 18:11:04 -0800

    imprimir lsta

--- sesion_03.py
+++ sesion_03.py
@@ -19,3 +19,34 @@ mi_lista_2 = ["lunes", "martes", "miercoles", "jueves", "viernes"]
 for i in mi_lista_2:
     if i !="lunes":
        print (f"Feliz {i}!")
+
+
+
+#Dada la lista my_list_2 = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"]
+#Imprime cada elemento de la lista 3 veces y cuando sea lunes no la incluyas.
+#Pista: usa los tipos de loops while y for en el mismo programa para lograrlo
+#Resultado:
+#Martes
+#Miercoles
+#Jueves
+#Viernes
+#Martes
+#Miercoles
+#Jueves
+#Viernes
+#Martes
+#Miercoles
+#Jueves
+#Viernes
+
+mi_lista_2 = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"]
+
+for lista in mi_lista_2:
+    if lista == "Lunes":
+        continue
+
+    i = 0
+
+    while i < 3:
+        print(lista)
+        i += 1
