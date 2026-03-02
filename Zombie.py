de14015f9cca0d7ee00c4da5e2815b354addfe59
Acxa <anavillavillaana@gmail.com>
2026-03-02 15:42:49 -0800

    zimbie se hizo glowup

--- Zombie.py
+++ Zombie.py
@@ -1,5 +1,5 @@
 from Enemigo import *
-
+import random
 class Zombie(Enemigo):
     def __init__(self, puntos_energia =10, ataque = 1):
         super().__init__(tipo_enemigo = 'Zombie', puntos_energia =puntos_energia, ataque = ataque)
@@ -7,5 +7,13 @@ class Zombie(Enemigo):
     def habla(self):
             print("Hummmm...*")
 
+
     def programar_enfermedad(self):
         print("El Zombie esta tratando depropagar la enfermedad!!")
+    
+    def ataque_especial(self):
+        print ("Zombie ataque especial")
+        funciona_ataque_especial = random.random() < 0.50
+        if funciona_ataque_especial:
+            self.puntos_energia += 2
+            print ("Zombie ha regenerado su energia con 2HP!!")
