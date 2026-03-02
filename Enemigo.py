b26059ec9862ce8b9c61f81803f32a623ea50e04
Acxa <anavillavillaana@gmail.com>
2026-03-02 15:58:33 -0800

    Cambiemuchas cosas

--- Enemigo.py
+++ Enemigo.py
@@ -19,3 +19,6 @@ def camina (self):
 
 def atacar(self):
     print(f"{self.__tipo_enemigo} ataca con un {self.ataque} de daño!!")
+
+def ataque_especial(self):
+     print("Enemigo no tiene ataque especial")
